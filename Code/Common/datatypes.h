7a23a3c2a176479fce29b292a64db6a45149e187
yapingxin <xinyp@live.com>
2025-12-03 19:00:40 +0800

    Upload Common/datatypes.h

---
+++ Code/Common/datatypes.h
@@ -0,0 +1,39 @@
+﻿/*****************************************************************************
+ *                                                                           *
+ *  @file     Common/datatypes.h                                             *
+ *  @brief    Common data types definitions.                                 *
+ *                                                                           *
+ *  @author   Yaping Xin <xinyp@live.com>                                    *
+ *  @link     https://github.com/yapingxin                                   *
+ *  @version  0.1.0.0                                                        *
+ *  @date     2025-12-03                                                     *
+ *                                                                           *
+ *  Copyright 2025 Yaping Xin                                                *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *  http://www.apache.org/licenses/LICENSE-2.0                               *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ *  Remark         : Description                                             *
+ *---------------------------------------------------------------------------*
+ *  Change History :                                                         *
+ *  <Date>     | <Version> | <Author>       | <Description>                  *
+ *---------------------------------------------------------------------------*
+ *  2025/12/03 | 0.1.0.0   | Yaping Xin     | Create file                    *
+ *---------------------------------------------------------------------------*
+ *                                                                           *
+ *************************************************************************** */
+
+#ifndef _INC_COMMON_datatypes_H
+#define _INC_COMMON_datatypes_H
+
+#endif // !_INC_COMMON_datatypes_H
