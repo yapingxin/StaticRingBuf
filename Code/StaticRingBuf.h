631ab144e92a9e31e4e08d855ebe9e2b4fb88291
yapingxin <xinyp@live.com>
2025-12-04 09:52:42 +0800

    Create StaticRingBuf.h

---
+++ Code/StaticRingBuf.h
@@ -0,0 +1,36 @@
+﻿/*****************************************************************************
+ *  @file     StaticRingBuf.h                                                *
+ *  @brief    Interface definition of the Static Ring Buffer.                *
+ *                                                                           *
+ *  @author   Yaping Xin                                                     *
+ *  @link     https://github.com/yapingxin                                   *
+ *  @version  0.1.0.0                                                        *
+ *  @date     2025-12-04                                                     *
+ *---------------------------------------------------------------------------*
+ *  Copyright 2025 Yaping Xin                                                *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *---------------------------------------------------------------------------*
+ *  Change History :                                                         *
+ *---------------------------------------------------------------------------*
+ *  2025/12/04 | 0.1.0.0   | Yaping Xin     | Create file                    *
+ *---------------------------------------------------------------------------*
+ *                                                                           *
+ *************************************************************************** */
+
+#ifndef _INC_GH2025_StaticRingBuf_H
+#define _INC_GH2025_StaticRingBuf_H
+
+#include "Common/datatypes.h"
+
+#endif // !_INC_GH2025_StaticRingBuf_H
