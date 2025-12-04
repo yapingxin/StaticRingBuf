4d44d35f80c714abae95013022f6cad9fbdcaa49
yapingxin <xinyp@live.com>
2025-12-04 11:20:06 +0800

    Create MSVC project Examples/rbuf_basic/rbuf_basic.vcxproj

---
+++ Examples/rbuf_basic/rbuf_basic.c
@@ -0,0 +1,10 @@
+﻿// rbuf_basic.c : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    return EXIT_SUCCESS;
+}
